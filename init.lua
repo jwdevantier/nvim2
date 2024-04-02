fdb250244b86d081ddbc72f81b1534d878e27493
Jesper Wendel Devantier <jwd@defmacro.it>
2024-04-02 14:05:13 +0200

    configure go/delve debugger

--- init.lua
+++ init.lua
@@ -243,6 +243,43 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>dq', '<CMD>DapTerminate<CR>', { desc = '[D]ebug [Q]uit' })
       vim.keymap.set('n', '<leader>dR', ':lua require("dapui").open({reset = true})<CR>', { desc = '[D]ebug UI [R]eset' })
 
+      -- GO SECTION -------------------------------
+      -- Source:
+      -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#go-using-delve-directly
+      dap.adapters.delve = {
+        type = 'server',
+        port = '${port}',
+        executable = {
+          command = 'dlv',
+          args = {'dap', '-l', '127.0.0.1:${port}'},
+        }
+      }
+
+      -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}"
+        },
+        {
+          type = "delve",
+          name = "Debug test", -- configuration for debugging test files
+          request = "launch",
+          mode = "test",
+          program = "${file}"
+        },
+        -- works with go.mod packages and sub packages
+        {
+          type = "delve",
+          name = "Debug test (go.mod)",
+          request = "launch",
+          mode = "test",
+          program = "./${relativeFileDirname}"
+        }
+      }
+      -- /GO SECTION -------------------------------
     end
   },
 
